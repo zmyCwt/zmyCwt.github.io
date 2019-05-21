dcf7ee2494bfa907b35745658ce91f9375415113
zmyCwt <48055283+zmyCwt@users.noreply.github.com>
2019-05-21 22:00:36 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//  function soso() {
+//     var wb = document.getElementById("sou");
+//     if (wb.value == "") {
+//         alert("你搜的啥玩意");
+//         } else {
+//         var url = "https://www.baidu.com/s?ie=UTF-8&wd=" + wb.value;
+//         window.location.href = url;
+//         	}
+
+//         }
+// 	<div class="content">
+//         <input type="text" value="" id="sou" class="sou"/>
+//         <button onclick="soso()" class="anniu">搜索</button>
+// 	</div>
+	
+// function 
